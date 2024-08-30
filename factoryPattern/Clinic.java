51c7158f7ce852c0fe661278856704acee30a1a1
Renard B. Macorol <84180143+RenardMacorol@users.noreply.github.com>
2024-08-30 03:17:56 +0000

    Edit and Finalize

--- factoryPattern/Clinic.java
+++ factoryPattern/Clinic.java
@@ -6,34 +6,54 @@ public class Clinic {
     public static void main(String[] args){
 
         Scanner input = new Scanner(System.in);
+        Integer choice =0 ;
 
+
+       while(choice!=3){
         System.out.println("[1] Dog");
         System.out.println("[2] Cat");
+        System.out.println("[3] Exit");
         System.out.print("\nChoose your pet number: ");
-        Integer choice = input.nextInt();
-
-        PetRecord petFile = new PetRecord();
-        Pet pet;
-
-        switch(choice){
+        choice = input.nextInt();
+            PetRecord petFile = new PetRecord();
+            Pet pet;
+            switch(choice){
             case 1: pet = new Dog();
                 petFile.setPetId("D01");
                 petFile.setPetName("Bantay");
                 petFile.setPet(pet);
                 ((Dog) pet).setBreed("German Shepperd");
+                
+                print(petFile);
                 break;
             case 2: pet = new Cat();
                 petFile.setPetId("C01");
                 petFile.setPetName("Muning");
                 petFile.setPet(pet);
                 ((Cat) pet).setNoOfLives(9);
+                print(petFile);
+                break;
+            }
+        
         }
+        input.close();
+    }
 
+    public static void print(PetRecord petFile){
         System.out.println("Pet id is " + petFile.getPetId());
         System.out.println("Pet name is " + petFile.getPetName());
         System.out.println("Pet kind: " + petFile.getPet().getClass().getSimpleName());
         System.out.println("Communication sound: "+ petFile.getPet().makeSound());
         System.out.println("Play mode: " + petFile.getPet().play());
-
+        switch (petFile.getPet().getClass().getSimpleName()) {
+            case "Cat":
+                System.out.println("Number of lives: "+((Cat)petFile.getPet()).getNoOfLives());
+                break;
+            case "Dog":
+                
+                System.out.println("Breed:"+((Dog) petFile.getPet()).getBreed());
+                break;
+        }
+        System.out.println();
     }
 }
